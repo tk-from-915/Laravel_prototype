65969d3f0691f2483b464db286290827ed5a097a
toki-kaori <k.toki.biz@gmail.com>
2020-09-11 09:33:09 +0900

    簡単sns作成

--- app/Http/Controllers/Auth/TimelineController.php
+++ app/Http/Controllers/Auth/TimelineController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use \App\Tweet;
 
 class TimelineController extends Controller
 {
     public function showTimelinePage()
     {
-        return view('auth.timeline'); // resource/views/auth/timeline.blade.phpを表示する
+        $tweets = Tweet::latest()->get();
+        return view('auth.timeline', compact('tweets')); // resource/views/auth/timeline.blade.phpを表示する
     }
 
-    public function postTweet(Request $request) //ここはあとで実装します。(Requestはpostリクエストを取得するためのものです。)
+    public function postTweet(Request $request)
     {
-
+        $validator = $request->validate([ // これだけでバリデーションできるLaravelすごい！
+            'tweet' => ['required', 'string', 'max:280'], // 必須・文字であること・280文字まで（ツイッターに合わせた）というバリデーションをします（ビューでも軽く説明します。）
+        ]);
+        Tweet::create([ // tweetテーブルに入れるよーっていう合図
+            'user_id' => Auth::user()->id, // Auth::user()は、現在ログインしている人（つまりツイートしたユーザー）
+            'tweet' => $request->tweet, // ツイート内容
+        ]);
+        return back(); // リクエスト送ったページに戻る（つまり、/timelineにリダイレクトする）
     }
 }
--- resources/views/auth/timeline.blade.php
+++ resources/views/auth/timeline.blade.php
@@ -10,15 +10,23 @@
     </head>
     <body  style="height:100%; background-color: #E6ECF0;">
         <div class="wrapper"  style="margin: 0 auto; width: 50%; height: 100%; background-color: white;">
-
-        <form action="/timeline" method="post">
-            {{ csrf_field() }}
-            <div style="background-color: #E8F4FA; text-align: center;">
-                <input type="text" name="tweet" style="margin: 1rem; padding: 0 1rem; width: 70%; border-radius: 6px; border: 1px solid #ccc; height: 2.3rem;" placeholder="今どうしてる？">
-                <button type="submit" style="background-color: #2695E0; color: white; border-radius: 10px; padding: 0.5rem;">ツイート</button>
+            <form action="/timeline" method="post">
+                {{ csrf_field() }}
+                <div style="background-color: #E8F4FA; text-align: center;">
+                    <input type="text" name="tweet" style="margin: 1rem; padding: 0 1rem; width: 70%; border-radius: 6px; border: 1px solid #ccc; height: 2.3rem;" placeholder="今どうしてる？">
+                    <button type="submit" style="background-color: #2695E0; color: white; border-radius: 10px; padding: 0.5rem;">ツイート</button>
+                </div>
+                @if($errors->first('tweet')) <!-- 追加 -->
+                        <p style="font-size: 0.7rem; color: red; padding: 0 2rem;">※{{$errors->first('tweet')}}</p>
+                @endif
+            </form>
+            <div class="tweet-wrapper"> <!-- この辺追加 -->
+                @foreach($tweets as $tweet)
+                <div style="padding:2rem; border-top: solid 1px #E6ECF0; border-bottom: solid 1px #E6ECF0;">
+                    <div>{{ $tweet->tweet }}</div>
+                </div>
+                @endforeach
             </div>
-        </form>
-
         </div>
         <script src="{{ mix('js/app.js') }}"></script>
     </body>
