2d86dfae69633143a1d43913129c3ad75ef26dd1
toki-kaori <k.toki.biz@gmail.com>
2020-11-04 14:52:42 +0900

    ユーザ登録時にauthorityも一緒に登録できるようにした

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255','unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'authority' => ['required', 'integer'],
         ]);
     }
 
@@ -67,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'authority' => $data['authority'],
         ]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,15 +7,31 @@
 @section('auth_login_title', 'Create your Account')
 
 @section('contents')
+<form method="POST" action="{{ route('register') }}">
+@csrf
     <p>user_name</p>
-        <input type="text">
+        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+        @if ($errors->has('name'))
+            <p class="red">{{ $errors->first('name') }}</p>
+        @endif
     <p>mail_adress</p>
-        <input type="email">
+        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+        @if ($errors->has('email'))
+            <p class="red">{{ $errors->first('email') }}</p>
+        @endif
     <p>password</p>
-        <input type="password">
+        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+        @if ($errors->has('password'))
+            <p class="red">{{ $errors->first('password') }}</p>
+        @endif
     <p>confirm password</p>
-        <input type="password">
-    <p>Authority</p>
-        <input type="password">
-    <input type="submit" name="login_submit" id="login_submit" class="green_button" value="Sign up">
+        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+    <p>User_authority</p>
+        <select name="authority">
+            <option value="0">投稿者</option>
+            <option value="1">管理者</option>
+        </select>
+    <button type="submit" id="login_submit" class="green_button">
+        {{ __('Sign up') }}
+    </button>
 @endsection
