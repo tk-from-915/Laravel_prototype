1e2eb31ec314b2f10c6426fa5f1307de072c992a
toki-kaori <k.toki.biz@gmail.com>
2020-11-20 19:44:49 +0900

    管理画面のhomeに各投稿記事の最新３件ずつ表示

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -12,8 +13,34 @@ class AdminController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * 管理画面homeを表示する
+     *
+     */
     public function home()
     {
-        return view('admin.home');
+        $data =[
+            'menu_posts' => self::getEachPosts(0),
+            'news_posts' => self::getEachPosts(1),
+            'blog_posts' => self::getEachPosts(2),
+        ];
+        return view('admin.home',$data);
+    }
+
+    /**
+     * 最近更新した記事を３件取得する
+     *
+     * @param  $post_type
+     */
+    public function getEachPosts($post_type)
+    {
+        return DB::table('users')
+                ->join('posts', function ($join) use( $post_type ){
+                    $join->on('users.id', '=', 'posts.user_id')   
+                        ->where('posts.post_type', '=', $post_type);
+                })
+                ->orderBy('posts.updated_at', 'desc')
+                ->take(3)
+                ->get();
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,25 +6,25 @@
     <div class="admin_page_title">最新Menu情報</div>
     <div class="info_block">
         <ul>
-            <li>2020/05/11    テストユーザ１さんによって モンステラ が更新されました。</li>
-            <li>2020/07/18    tokiさんによって 多肉植物 が更新されました。</li>
-            <li>2020/10/21    テストユーザ３さんによって テラリウム が更新されました。</li>
+        @foreach($menu_posts as $menu_post)
+            <li>{{ $menu_post->updated_at }}   {{ $menu_post->name }}さんによって{{ $menu_post->post_title }}が更新されました。</li>
+        @endforeach
         </ul>
     </div>
     <div class="admin_page_title">最新News情報</div>
     <div class="info_block">
         <ul>
-            <li>2020/05/11    テストユーザ１さんによって モンステラ が更新されました。</li>
-            <li>2020/07/18    tokiさんによって 多肉植物 が更新されました。</li>
-            <li>2020/10/21    テストユーザ３さんによって テラリウム が更新されました。</li>
+        @foreach($news_posts as $news_post)
+            <li>{{ $news_post->updated_at }}   {{ $news_post->name }}さんによって{{ $news_post->post_title }}が更新されました。</li>
+        @endforeach
         </ul>
     </div>
     <div class="admin_page_title">最新Blog情報</div>
     <div class="info_block">
         <ul>
-            <li>2020/05/11    テストユーザ１さんによって モンステラ が更新されました。</li>
-            <li>2020/07/18    tokiさんによって 多肉植物 が更新されました。</li>
-            <li>2020/10/21    テストユーザ３さんによって テラリウム が更新されました。</li>
+        @foreach($blog_posts as $blog_post)
+            <li>{{ $blog_post->updated_at }}   {{ $blog_post->name }}さんによって{{ $blog_post->post_title }}が更新されました。</li>
+        @endforeach
         </ul>
     </div>
     <div class="admin_page_title">最新お問い合わせ情報</div>
