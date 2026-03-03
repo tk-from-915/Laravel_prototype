2dd52ab284fccded62ad1a56ff212b40c438cca1
TOKI <k.toki.biz@gmail.com>
2026-03-03 15:16:06 +0900

    add:aboutUs

--- hp/components/common/AppHeader.vue
+++ hp/components/common/AppHeader.vue
@@ -12,7 +12,7 @@ const { isNavOpen, toggleNav, closeNav } = useNavigation()
     <nav id="sidebar">
       <ul id="sidebar_list">
         <li class="sidebar__item"><NuxtLink to="/" @click="closeNav">トップ</NuxtLink></li>
-        <li class="sidebar__item"><NuxtLink to="/about" @click="closeNav">私たちについて</NuxtLink></li>
+        <li class="sidebar__item"><NuxtLink to="/aboutUs" @click="closeNav">私たちについて</NuxtLink></li>
         <li class="sidebar__item"><NuxtLink to="/company" @click="closeNav">会社情報</NuxtLink></li>
         <li class="sidebar__item"><NuxtLink to="/products" @click="closeNav">商品一覧</NuxtLink></li>
         <li class="sidebar__item"><NuxtLink to="/blog" @click="closeNav">News</NuxtLink></li>
